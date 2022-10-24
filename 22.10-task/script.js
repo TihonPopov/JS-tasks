b1e9179111998c0b7b7cd5719422c6e4c60852e5
HanTengri <obivankenobi1992@gmail.com>
2022-10-24 09:08:30 +0600

    fix:script

--- 22.10-task/script.js
+++ 22.10-task/script.js
@@ -1,4 +1,6 @@
 let PervyiaCifra = 0;
 let vtorayaCivfra = 1;
 let tretya_cifra = 2;
-alert(PervyiaCifra,vtorayaCivfra,tretya_cifra )
+alert(PervyiaCifra);
+alert(vtorayaCivfra);
+alert(tretya_cifra);
