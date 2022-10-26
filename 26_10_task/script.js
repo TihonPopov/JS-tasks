1bac7d4a60cec27d4345bbc95df6d9f1b6708122
HanTengri <obivankenobi1992@gmail.com>
2022-10-26 10:45:45 +0600

    feat: 26-10 task

---
+++ 26_10_task/script.js
@@ -0,0 +1,77 @@
+// let num = 10;
+// num = ++num;
+// num = num++;
+// num = --num;
+// console.log(num);
+
+
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+
+// let num = 1;
+//   num += 12;
+//   num -= 14;
+//   num *= 5;
+//   num /= 7;
+//   num = ++num;
+//   num = --num;
+//   alert(num);
+
+// &&
+
+// all true
+
+// let user = "kamel";
+// let age = 20;
+// let company = 'firefox';
+// let result = user === 'kamel' && age === 20 && company === 'firefox'
+// console.log(result);
+
+// one false
+
+// let user = "kamel";
+// let age = 20;
+// let company = 'firefox';
+// let result = user === 'kamel' && age === 24 && company === 'firefox'
+// console.log(result);
+
+// all false
+
+// let user = "kamel";
+// let age = 20;
+// let company = 'firefox';
+// let result = user === 'frog' && age === 24 && company === 'IE'
+// console.log(result);
+
+
+
+//  ||
+
+
+// one false
+
+// let user = "kamel";
+// let age = 20;
+// let company = 'firefox';
+// let result = user === 'kamel' || age === 24 || company === 'firefox'
+// console.log(result);
+
+// all false
+
+// let user = "kamel";
+// let age = 20;
+// let company = 'firefox';
+// let result = user === 'dog' && age === 24 && company === 'opera'
+// console.log(result);
+
+// all true
+let user = "kamel";
+let age = 20;
+let company = 'firefox';
+let result = user === 'kamel' && age === 20 && company === 'firefox'
+console.log(result);
