e25d48927a461d129745d6fc332f05fda4891bf9
HanTengri <obivankenobi1992@gmail.com>
2022-11-01 11:28:45 +0600

    feat: new Array

--- 01_11_task/script.js
+++ 01_11_task/script.js
@@ -37,7 +37,9 @@ let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 // let odd = 0;
 for( let i = 0; i < array.length; i++){
    if ((array[i]%2) === 0) {
-    console.log(array[i]);
+    // console.log(array[i]);
+    const newArray = [];
+    newArray.push(array[i]);
+    console.log(newArray);
    } 
-    
 }
